7b79f80ef847fd1864f6edbef448c52dfb7156b5
wenguang pan <wenguangpan@wenguangdeMacBook-Pro.local>
2017-03-28 21:05:07 +0800

    objc_msgSend直接调用

--- YYModel-Anls/main.m
+++ YYModel-Anls/main.m
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <objc/message.h>
+
 @interface NSObject (Sark)
 + (void)foo;
 @end
@@ -20,9 +22,13 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        // http://chun.tips/2014/11/06/objc-runtime-3/ (刨根问底 Objective－C Runtime（3）－ 消息 和 Category)
         // 用clang -rewrite-objc main.m 后，以下的对应C++语句
         // ((void (*)(id, SEL))(void *)objc_msgSend)((id)objc_getClass("NSObject"), sel_registerName("foo"));
+        // 以下3句是等价的
         [NSObject foo];
+        ((void (*)(id, SEL))objc_msgSend)((id)objc_getClass("NSObject"), sel_registerName("foo"));
+        ((void (*)(id, SEL))(void *)objc_msgSend)((id)objc_getClass("NSObject"), sel_registerName("foo"));
     }
     return 0;
 }
